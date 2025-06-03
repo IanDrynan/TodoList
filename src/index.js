7bf6b0f09be1436cf2b5e4662105fb554df2353e
Ian <drynanian@gmail.com>
2025-06-02 17:35:06 -0700

    Click new todo to display a modal to add a new Todo.
    Next need to update the page so that it displays all the todos of the current project

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ function getData() {
 
   JSON.parse(localStorage.getItem("projects"));
 }
-
-const projects = ["inbox"];
 const inbox_project = new Project("inbox");
+const projectMap = new Map();
+projectMap.set("inbox", inbox_project);
 const test_todo = new Todo("test", "desc", "1", "2020");
 inbox_project.addTodoToProject(test_todo);
 inbox(inbox_project);
@@ -27,31 +27,41 @@ newTodo.addEventListener("click", () => {
   const dialog = document.createElement("dialog");
   dialog.id = "newTodoDialog";
   document.body.appendChild(dialog);
-  dialog.innerHTML =
-        `<form>
+  dialog.innerHTML = `<form>
             <label for="name">Name</label>
             <input type="text" name="name" id="name" required>
             <label for="description">Description</label>
             <input type="text" name="description" id="description">
             <label for="priority">Priority</label>
-            <select>
+            <select id="priority">
                 <option value="p1">p1</option>
                 <option value="p2">p2</option>
                 <option value="p3">p3</option>
             </select>
             <label for="dueDate">Due Date</label>
             <input type="date" name="dueDate" id="dueDate">
+            <label for="project">Project</label>
+            <select id="selectProject">
+            </select>
             <button type="submit" id="confirmNewTodo">Confirm</button>
             <button type="submit" id="cancel">Cancel</button>
         </form>`;
+  for (const project of projectMap.keys()) {
+    const option = document.createElement("option");
+    option.value = project;
+    option.textContent = project;
+    dialog.querySelector("#selectProject").appendChild(option);
+  }
   dialog.showModal();
-});
-const newTodoDialog = document.getElementById("newTodoDialog");
-const newTodoName = newTodoDialog.querySelector("#name");
-const newTodoDescription = newTodoDialog.querySelector("#description");
-const newTodoPriority = newTodoDialog.querySelector("select");
-const confirmNewTodo = newTodoDialog.querySelector("#confirmNew");
-confirmNewTodo.addEventListener("click", (event) => {
+
+  const newTodoDialog = document.getElementById("newTodoDialog");
+  const newTodoName = newTodoDialog.querySelector("#name");
+  const newTodoDescription = newTodoDialog.querySelector("#description");
+  const newTodoPriority = newTodoDialog.querySelector("#priority");
+  const newTodoDueDate = newTodoDialog.querySelector("#dueDate");
+  const newTodoProject = newTodoDialog.querySelector("#selectProject");
+  const confirmNewTodo = newTodoDialog.querySelector("#confirmNewTodo");
+  confirmNewTodo.addEventListener("click", (event) => {
     event.preventDefault();
     const requiredFields = newTodoDialog.querySelectorAll("[required]");
     let valid = true;
@@ -59,12 +69,25 @@ confirmNewTodo.addEventListener("click", (event) => {
       if (!field.value) {
         valid = false;
       }
-    })
+    });
     if (valid) {
-
+        const todo = new Todo(
+            newTodoName.value,
+            newTodoDescription.value,
+            newTodoPriority.value,
+            newTodoDueDate.value,
+            newTodoProject.value
+        );
+        projectMap.get(newTodoProject.value).addTodoToProject(todo);
+        inbox(inbox_project);
+        newTodoDialog.close();
     }
-  const todo = new Todo(newTodoName.value, newTodoDescription.value, newTodoPriority.value, "2020");
-  inbox_project.addTodoToProject(todo);
-  inbox(inbox_project);
-  newTodoDialog.close();
+    else {
+        alert("Please fill in all fields");
+    }
+  });
+  const cancel = newTodoDialog.querySelector("#cancel");
+  cancel.addEventListener("click", () => {
+    newTodoDialog.close();
+  });
 });
