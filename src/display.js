46977c617b68f08a31e2ed6263c67d68acbbfe02
Ian <drynanian@gmail.com>
2025-06-03 16:05:48 -0700

    Refactor modal into html. Rename inbox.js to display.js

--- src/display.js
+++ src/display.js
@@ -1,11 +1,11 @@
 const main = document.querySelector("#main");
 const mainBtns = document.querySelector("#mainBtns");
 
-const inbox = (project) => {
+const display = (project) => {
   const inboxDiv = document.createElement("div");
   inboxDiv.id = "inbox";
   main.insertBefore(inboxDiv, mainBtns);
   console.log(project.todos);
 };
 
-export { inbox };
+export { display };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import { inbox } from "./inbox.js";
+import { display } from "./display.js";
 import { Project } from "./project.js";
 import { Todo } from "./todo.js";
 
@@ -15,37 +15,18 @@ function getData() {
 
   JSON.parse(localStorage.getItem("projects"));
 }
+//init data
 const inbox_project = new Project("inbox");
 const projectMap = new Map();
 projectMap.set("inbox", inbox_project);
-const test_todo = new Todo("test", "desc", "1", "2020");
+//placeholder data including default project inbox
+const test_todo = new Todo("test", "desc", "1");
 inbox_project.addTodoToProject(test_todo);
-inbox(inbox_project);
-
+display(inbox_project);
+//New Todo button event listener
+const dialog = document.getElementById("newTodoDialog");
 const newTodo = document.getElementById("newTodoBtn");
 newTodo.addEventListener("click", () => {
-  const dialog = document.createElement("dialog");
-  dialog.id = "newTodoDialog";
-  document.body.appendChild(dialog);
-  dialog.innerHTML = `<form>
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" required>
-            <label for="description">Description</label>
-            <input type="text" name="description" id="description">
-            <label for="priority">Priority</label>
-            <select id="priority">
-                <option value="p1">p1</option>
-                <option value="p2">p2</option>
-                <option value="p3">p3</option>
-            </select>
-            <label for="dueDate">Due Date</label>
-            <input type="date" name="dueDate" id="dueDate">
-            <label for="project">Project</label>
-            <select id="selectProject">
-            </select>
-            <button type="submit" id="confirmNewTodo">Confirm</button>
-            <button type="submit" id="cancel">Cancel</button>
-        </form>`;
   for (const project of projectMap.keys()) {
     const option = document.createElement("option");
     option.value = project;
@@ -53,41 +34,41 @@ newTodo.addEventListener("click", () => {
     dialog.querySelector("#selectProject").appendChild(option);
   }
   dialog.showModal();
-
-  const newTodoDialog = document.getElementById("newTodoDialog");
-  const newTodoName = newTodoDialog.querySelector("#name");
-  const newTodoDescription = newTodoDialog.querySelector("#description");
-  const newTodoPriority = newTodoDialog.querySelector("#priority");
-  const newTodoDueDate = newTodoDialog.querySelector("#dueDate");
-  const newTodoProject = newTodoDialog.querySelector("#selectProject");
-  const confirmNewTodo = newTodoDialog.querySelector("#confirmNewTodo");
-  confirmNewTodo.addEventListener("click", (event) => {
-    event.preventDefault();
-    const requiredFields = newTodoDialog.querySelectorAll("[required]");
-    let valid = true;
-    requiredFields.forEach((field) => {
-      if (!field.value) {
-        valid = false;
-      }
-    });
-    if (valid) {
-        const todo = new Todo(
-            newTodoName.value,
-            newTodoDescription.value,
-            newTodoPriority.value,
-            newTodoDueDate.value,
-            newTodoProject.value
-        );
-        projectMap.get(newTodoProject.value).addTodoToProject(todo);
-        inbox(inbox_project);
-        newTodoDialog.close();
-    }
-    else {
-        alert("Please fill in all fields");
+});
+//Create new todo from dialog
+const newTodoDialog = document.getElementById("newTodoDialog");
+const newTodoName = newTodoDialog.querySelector("#name");
+const newTodoDescription = newTodoDialog.querySelector("#description");
+const newTodoPriority = newTodoDialog.querySelector("#priority");
+const newTodoDueDate = newTodoDialog.querySelector("#dueDate");
+const newTodoProject = newTodoDialog.querySelector("#selectProject");
+const confirmNewTodo = newTodoDialog.querySelector("#confirmNewTodo");
+confirmNewTodo.addEventListener("click", (event) => {
+  event.preventDefault();
+  const requiredFields = newTodoDialog.querySelectorAll("[required]");
+  let valid = true;
+  requiredFields.forEach((field) => {
+    if (!field.value) {
+      valid = false;
     }
   });
-  const cancel = newTodoDialog.querySelector("#cancel");
-  cancel.addEventListener("click", () => {
+  if (valid) {
+    const todo = new Todo(
+      newTodoName.value,
+      newTodoDescription.value,
+      newTodoPriority.value,
+      newTodoDueDate.value
+    );
+    projectMap.get(newTodoProject.value).addTodoToProject(todo);
+    //update ui
     newTodoDialog.close();
-  });
+  } else {
+    alert("Please fill in all fields");
+  }
+});
+//Cancel new todo
+const cancel = newTodoDialog.querySelector("#cancel");
+cancel.addEventListener("click", (event) => {
+  event.preventDefault();
+  newTodoDialog.close();
 });
